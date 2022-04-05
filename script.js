16f79ce443103dc509cb8e2953775ecd09f3c78c
Harneet Singh <Harneetchanni.singh@gmail.com>
2022-04-05 15:48:49 -0400

    added progress bar updating

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img')
 const title = document.getElementById('title');
 const artist = document.getElementById('artist')
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -93,8 +95,23 @@ function nextSong() {
 loadSong(songs[0]);
 
 
+// Update Progress Bar & Time 
+
+function updateProgressBar(e){
+    if (isPlaying){
+        const {duration, currentTime } = e.srcElement; 
+        // Update Progress Bar 
+
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+
+    }
+}
+
+
 // Event Listeners 
 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar)
 
